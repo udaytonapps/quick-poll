fa4df7ff5abf8457596da93b1d43b276bbe40797
Andrew Niekamp <aniekamp1@udayton.edu>
2022-05-26 10:05:47 -0400

    TSUGI-308 Add images and update description

--- register.php
+++ register.php
@@ -4,7 +4,7 @@ $REGISTER_LTI2 = array(
     "name" => "Quick Poll",
     "FontAwesome" => "fa-bar-chart-o",
     "short_name" => "Quick Poll",
-    "description" => "This app allows an instructor to ask users poll questions.",
+    "description" => "Create a poll with a question and answer choices. Available settings include allowing students to see results and making poll results anonymous.",
     "messages" => array("launch"),
     "privacy_level" => "public",  // anonymous, name_only, public
     "license" => "Apache",
@@ -14,8 +14,15 @@ $REGISTER_LTI2 = array(
     "analytics" => array(
         "internal"
     ),
-    "source_url" => "https://github.com/udaytonapps/quickpoll",
+    "video" => "https://udayton.warpwire.com/w/HfsEAA/",
+    // "source_url" => "https://github.com/udaytonapps/quickpoll",
     "screen_shots" => array(
+        "images/QP-Instructor-Getting-Started.jpg",
+        "images/QP-Add-Poll.jpg",
+        "images/QP-Student-View.jpg",
+        "images/QP-Instructor-Responses.jpg",
+        "images/QP-Instructor-Results.jpg",
+        "images/QP-Student-View-Responded.jpg",
     ),
     "tool_phase" => "New"
 );
